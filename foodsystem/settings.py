153e5d2aeb8ab7ce9410e5459f148aaf242bf04f
Niwael Mavella <niwaelmavella@gmail.com>
2026-03-26 16:17:08 +0300

    foodsystem

--- foodsystem/settings.py
+++ foodsystem/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+#AUTH_USER_MODEL = 'users.User' Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
+
--- users/views.py
+++ users/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, authenticate
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
 def home(request):
     return render(request, 'home.html')
 def register(request):
     if request.method =="POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request,"Account created successfully")
             return redirect('login')
     else:
-           form=UserCreationForm()
-    return render(request,"register.html",{"form":form})
+           form= CustomUserCreationForm()
+    return render(request,"register.html",{"form": form})
 def login_view(request):
     if request.method == "POST":
         username=request.POST.get("username")
@@ -20,7 +23,14 @@ def login_view(request):
         user=authenticate(request, username=username,password=password)
         if user is not None:
             login(request,user)
-            return redirect("home")
+        if user.role == 'customer':
+              return redirect("home")
+        elif user.role == 'vendor':
+                return redirect("vendor_dashboard")
+        elif user.role == 'delivery':
+                return redirect("delivery_dashboard")
+        elif user.role == 'admin':
+                return redirect("admin_dashboard")
 
     else:
          messages.error(request,"username or password is incorrect")
