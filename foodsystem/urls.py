7f236a78767dab3d6a0137fc36c576787ab00bed
Niwael <niwaelmavella@gmail.com>
2026-06-21 22:05:02 +0300

    urls patterns

--- foodsystem/urls.py
+++ foodsystem/urls.py
@@ -19,4 +19,6 @@ from django.urls import path,include
 urlpatterns=[
     path('admin',admin.site.urls),
     path('',include('users.urls')),
+    
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'), 
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,14 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
+from django.contrib.auth import views as auth_views
+from users import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
-    path('login/',views.login_view,name='login'),
+    path('login/', views.login_view, name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
 ]
